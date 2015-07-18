649519533505f1bcfad13c41618080d9d252c9ae
Martin Eismann <martin@eismann.cc>
2015-07-18 17:02:43 +0200

    Install Bundler after RVM in setup scripts

--- setup_shell.sh
+++ setup_shell.sh
@@ -46,8 +46,11 @@ cd $repo_dir \
   && git submodule update --init vim/bundle/Vundle.vim/ \
   && vim +PluginInstall +qall && echo "… done."
 
-echo "Installing RVM…"
+echo "Installing RVM and Bundler…"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
   curl -sSL https://get.rvm.io | bash -s stable --ruby && \
+  source "$HOME/.rvm/scripts/rvm" && \
+  # export PATH=$PATH:$HOME/.rvm/bin && \
+  gem install bundler && \
   echo "…done."
 
