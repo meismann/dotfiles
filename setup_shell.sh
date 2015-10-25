55e1184db80e268f0f1b1a0ce29626fb79dcbf1b
Martin Eismann <softwerk@eismann.cc>
2015-10-25 12:27:06 +0100

    Move creation of symlinks to its own setup file

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
 
-create_backup(){
-  file_name=$1
-  if [[ -e $file_name && ! -L $file_name ]]
-  then
-    echo "Creating backup for $file_name ($file_name.bkp)"
-    mv $file_name $file_name.bkp
-  else
-    return 1
-  fi
-}
-
-remove_symlink() {
-  file_name=$1
-  if [[ -L $file_name ]]
-  then
-    echo "Removing symlink $file_name (pointing to $(readlink $file_name))"
-    rm -fr $file_name
-  fi
-}
-
-prepare_setup_of() {
-  file_name=$1
-  create_backup $file_name || remove_symlink $file_name
-}
-
-repo_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-cd
-
-# Link whatever needs to go right into ~
-linkable_files=( .bash_profile .bash_aliases .bashrc .gitexcludes .vim .vimrc bin .gitconfig )
-for i in "${linkable_files[@]}"
-do
-  prepare_setup_of $i
-  echo "Creating symlink $i"
-  ln -s $repo_dir/${i#.} $i
-done
-
-echo "Sourcing the new .bash_profile…"
-source $HOME/.bash_profile && echo "…done."
-
 echo "Installung Vim plugins with Vundle…"
 cd $repo_dir \
   && git submodule update --init vim/bundle/Vundle.vim/ \
