5868921e7c46a8ed86fbfa75bdd69ee5acbd9170
Martin Eismann <softwerk@eismann.cc>
2015-10-25 12:18:44 +0100

    Add setup_linux

---
+++ setup_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get --yes install vim git tree silversearcher-ag postgresql pgadmin3 \
+  virtualbox vlc mysql-server mysql-client mysql-workbench xclip
+
+# missing: timeedition sublime skype
