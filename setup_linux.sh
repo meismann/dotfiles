63d81b271b8a197d5dcf2edfc56bc0b5e717c74e
Martin Eismann <softwerk@eismann.cc>
2017-10-10 16:32:45 +0200

    Add Sublime Text to linux installation

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Prepare apt-get for Sublime Text
+# Install the GPG key:
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# Ensure apt is set up to work with https sources:
+sudo apt-get install apt-transport-https
+# Select the Stable channel:
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+sudo apt-get update
 sudo apt-get --yes install vim git tree silversearcher-ag postgresql pgadmin3 \
   virtualbox vlc mysql-server mysql-client mysql-workbench xclip gnome-do gimp \
-  synaptic nodejs youtube-dl chromium-browser curl gitg libmysqlclient-dev
+  synaptic nodejs youtube-dl chromium-browser curl gitg libmysqlclient-dev \
+  sublime-text
 
-# missing: timeedition sublime skype
+# missing: timeedition skype
 echo 'Now customise the Shell…'
 
 WERKBANK_DIR=$HOME/werkbank
