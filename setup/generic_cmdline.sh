674bff48cb56dcd530002245c0c0a710834144b9
Martin Eismann <softwerk@eismann.cc>
2020-10-24 20:33:50 +0200

    Fix grep expression for Linux bash

--- setup/generic_cmdline.sh
+++ setup/generic_cmdline.sh
@@ -5,7 +5,7 @@ echo "Installung Vim plugins with Vim-Plug…" \
   && echo "… done installing Vim plugins."
 
 echo "Installing latest Ruby version ($latest_ruby_version)…" \
-  && latest_ruby_version=$(rbenv install -l | grep -e '^\d\.' | sort | tail -n 1) \
+  && latest_ruby_version=$(rbenv install -l | grep -E '^[0-9.]{3,}' | sort | tail -n 1) \
   && rbenv install -s $latest_ruby_version \
   && echo '… done installing latest Ruby version' \
   && rbenv global $latest_ruby_version \
