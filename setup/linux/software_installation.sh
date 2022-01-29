543223a693c6872c46f5f79420a2cc6d63118436
meismann-as <martin.eismann@aboutsource.net>
2022-01-29 08:57:13 +0100

    Switch from apt-get to yay

--- setup/linux/software_installation.sh
+++ setup/linux/software_installation.sh
@@ -53,20 +53,20 @@ packages=(
 
 # Prepare apt-get for Sublime Text
 # Install the GPG key:
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 # Ensure apt is set up to work with https sources:
-sudo apt-get install apt-transport-https
+#sudo apt-get install apt-transport-https
 # Select the Stable channel:
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+#echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -60)" ]; then
+#if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -60)" ]; then
   # The main script (run_setup.sh) will have just run that in most cases
-  sudo apt-get update
-fi
-sudo apt-get --yes install ${packages[@]} || (echo 'Software install failed!' && exit 1)
+  #sudo apt-get update
+#fi
+sudo yay --noconfirm -S ${packages[@]} || (echo 'Software install failed!' && exit 1)
 
 # Remove Amazon shit:
-sudo apt-get --yes remove unity-webapps-common
+#sudo apt-get --yes remove unity-webapps-common
 
 echo 'Installing rbenv and ruby-build plugin…'
 export PATH="\
