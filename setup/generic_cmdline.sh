eebd9790e816ac378c5236c3c14c719c95888b9b
Martin Eismann <softwerk@eismann.cc>
2024-10-10 11:31:26 +0200

    Update linux software install

--- setup/generic_cmdline.sh
+++ setup/generic_cmdline.sh
@@ -12,15 +12,7 @@ echo "Installung Vim plugins with Vim-Plug…" \
   && vim +PlugInstall +qall \
   && echo "… done installing Vim plugins."
 
-latest_ruby_version=$(rbenv install -l | grep -E '^[0-9.]{3,}' | sort | tail -n 1) \
-  && echo "Installing latest Ruby version ($latest_ruby_version)…" \
-  && rbenv install -s $latest_ruby_version \
-  && echo '… done installing latest Ruby version' \
-  && rbenv global $latest_ruby_version \
-  && eval "$(rbenv init -)" \
-  && gem install --no-doc bundler slim_lint
-
 # From https://pypi.org/project/pdfCropMargins/
-echo 'Installing pdf-crop-margins…' \
-  && pip3 install pdfCropMargins[gui] borgbackup[fuse] --user --upgrade --no-warn-script-location \
-  && echo '… done installing pdf-crop-margins.'
+# echo 'Installing pdf-crop-margins…' \
+#  && pip3 install pdfCropMargins[gui] borgbackup[fuse] --user --upgrade --no-warn-script-location \
+#  && echo '… done installing pdf-crop-margins.'
--- setup/linux/software_installation.sh
+++ setup/linux/software_installation.sh
@@ -1,9 +1,13 @@
 packages=(
-  chromium-browser
+  albert
   curl
+  flatpak
   gimp
   git
   gitg
+  github-desktop
+  gnome-shell-extension-manager
+  keepassxc
   # gnome-do
   #libmysqlclient-dev
   #mysql-client
@@ -15,38 +19,26 @@ packages=(
   #pgadmin3
   #postgresql
   silversearcher-ag
-  #sublime-text
   synaptic
   tree
   virtualbox
   #wine
-  #vim
   vlc
   xclip
   # ifconfig
   net-tools
-  rbenv
-  ruby-build
-
-  # Dependencies of rbenv-build:
-  autoconf
-  bison
-  build-essential
-  libffi-dev
-  libgdbm-dev
-  libncurses5-dev
-  libreadline-dev
-  libreadline-dev
-  libssl-dev
-  libyaml-dev
-  zlib1g-dev
+  nextcloud-desktop
+  ruby-full
+  signal-desktop
+  wget
+  yt-dlp
 
   # Dependencies of pdf-crop-margins
-  ghostscript
-  poppler-utils
-  python3-pip
-  python3-setuptools
-  python3-tk
+  # ghostscript
+  # poppler-utils
+  # python3-pip
+  # python3-setuptools
+  # python3-tk
 
   # Dependencies of borg backup with mount (https://docs.borgbase.com/restore/mount/)
   #fuse
@@ -54,28 +46,36 @@ packages=(
   #pkg-config
 )
 
-# Prepare apt for Sublime Text
-# Install the GPG key:
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-# Ensure apt is set up to work with https sources:
-sudo apt install apt-transport-https
-# Select the Stable channel:
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# Add repo for Albert
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_24.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_24.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
 
-if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -60)" ]; then
-  # The main script (run_setup.sh) will have just run that in most cases
-  sudo apt update
-fi
+# Add repo for Github client
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+
+# Add repo Signal client
+# 1. Install our official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# Add repo for yt-dlp
+sudo add-apt-repository ppa:tomtomtom/yt-dlp
+
+# Now install everything
+sudo apt update
 sudo apt --yes install ${packages[@]} || (echo 'Software install failed!' && exit 1)
 
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
-sudo apt update
-sudo apt install github-desktop
-
-# Install Signal
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-sudo apt update && sudo apt install signal-desktop
+# Install Flatpaks
+flatpak install -y \
+	eu.betterbird.Betterbird \
+	org.chromium.Chromium \
+	org.apache.directory.studio \
+	org.telegram.desktop \
+	io.gitlab.librewolf-community \
+	md.obsidian.Obsidian \
+	com.sublimetext.three \
+	org.mozilla.Thunderbird
