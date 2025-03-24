bab61da06504414199c7f4bec6ba456fbfc4d980
Martin Eismann <softwerk@eismann.cc>
2025-03-24 13:00:49 +0100

    Add proper sudoers editor

--- setup/linux/configuration.sh
+++ setup/linux/configuration.sh
@@ -1,2 +1,5 @@
 # Fixes errors with my git aliasing during completions that occur because the `git` command is not present but expected
 echo "__git_cmd_idx=1" >> /usr/share/bash-completion/completions/git
+
+# Sets proper editor for visudo
+sudo update-alternatives --config editor
