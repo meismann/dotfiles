741f1aa84ef174c01b662e7224939d46d645f184
Martin Eismann <softwerk@eismann.cc>
2026-05-02 14:18:52 +0200

    Change colorscheme to "shine"

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,21 +1,25 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
+if true then return {} end
 
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
 -- In your plugin files, you can:
 -- * add extra plugins
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
-if true then return {
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "shine",
+      colorscheme = "gruvbox",
     },
   },
-}
-end
 
--- aus example.lua
-return {
   -- change trouble config
   {
     "folke/trouble.nvim",
