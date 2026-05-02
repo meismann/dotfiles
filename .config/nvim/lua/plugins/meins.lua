fc5e33f0048ef124fb7ac7c88be0fd7d8f54152b
Martin Eismann <softwerk@eismann.cc>
2026-05-02 18:47:31 +0200

    Add luarocks

--- .config/nvim/lua/plugins/meins.lua
+++ .config/nvim/lua/plugins/meins.lua
@@ -25,6 +25,11 @@ if true then return {
       },
     },
   },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  }
 }
 end
 
