7aecbf997569a6e0713105b64981093e5e8adadf
Martin Eismann <softwerk@eismann.cc>
2026-05-02 15:01:25 +0200

    Add CR for finding Git files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<CR>", builtin.git_files, { desc = "Find Git files" })
