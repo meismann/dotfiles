e7ff5919f3830dc2bb6435819867139d73791be4
Martin Eismann <softwerk@eismann.cc>
2026-05-02 18:05:41 +0200

    Add telescope old files with BS and lspconfig

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<CR>", builtin.git_files, { desc = "Find Git files" })
+vim.keymap.set("n", "<BS>", builtin.oldfiles, { desc = "Find previously opened files" })
--- .config/nvim/lua/plugins/meins.lua
+++ .config/nvim/lua/plugins/meins.lua
@@ -11,6 +11,20 @@ if true then return {
       colorscheme = "shine",
     },
   },
+  {
+    "nvim-telescope/telescope.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        },
+      },
+    },
+  },
 }
 end
 
